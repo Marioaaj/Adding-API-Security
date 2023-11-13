bce9ec8ab7754c0364c5cee7cb10da8e5acddc20
Mario Aguilar <mario661962@yahoo.com>
2023-11-13 16:35:26 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+// Nodemon is a tool that helps develop Js/Nodejs based applications by automatically restarting the node application when file changes detected.
+// npm install nodemon --save-dev //
+// npx nodemon src/index.js
+// The comment above starts nodemon
+// nodemon executes the server and its waiting changes as soon as the server is saved or updated
+// eslint-disable-next-line no-console
+console.log('Hello There!')
+// npm install @babel/core @babel/cli @babel/preset-env @babel/node @babel/runtime @babel/plugin-transform-runtime --save-dev
